d05008c5e1ed7479d826999b3ce9265524f0c6fc
biswadeep101 <biswadeepmondal101@gmail.com>
2026-03-13 21:10:46 +0530

    fixing path

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 const port = process.env.PORT;
-const __dirname = path.resolve();
+const __dirname = path.resolve("../");
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
